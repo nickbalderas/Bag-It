e1b1e398820c9ad27657c8916d92f8b59ef51a9f
Nicholas Balderas <geist732@gmail.com>
2021-10-23 12:31:46 -0500

    Added game overlay. Various updates to bag, score manager, game manager scripts.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,22 +1,49 @@
+using TMPro;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    public static ScoreManager Instance;
-    private int Score { get; set; }
+    public int Score { get; private set; }
+    private int ScoreMultiplier { get; set; }
+    public TextMeshProUGUI scoreDisplay;
+    public TextMeshProUGUI scoreMultiplierDisplay;
 
-    private void Awake()
+    private void Start()
     {
-        if (Instance)
-        {
-            Destroy(gameObject);
-            return;
-        }
-        Instance = this;
+        Score = 0;
+        ScoreMultiplier = 1;
+        UpdateScoreDisplay();
+    }
+
+    public void UpdateScoreMultiplier(int newMultiplier)
+    {
+        ScoreMultiplier = newMultiplier;
+        UpdateScoreMultiplierDisplay();
     }
     
-    public void UpdateScore(int value, int multiplier)
+    public void UpdateScore(int value)
     {
-        Score += value * multiplier;
+        Score += value * ScoreMultiplier;
+        UpdateScoreDisplay();
+    }
+
+    private void UpdateScoreDisplay()
+    {
+        if (!scoreDisplay) return;
+        scoreDisplay.text = $"{Score}";
+    }
+
+    private void UpdateScoreMultiplierDisplay()
+    {
+        if (!scoreMultiplierDisplay) return;
+        scoreMultiplierDisplay.text = $"{"x" + ScoreMultiplier}";
+    }
+
+    public void Reset()
+    {
+        Score = 0;
+        ScoreMultiplier = 0;
+        scoreDisplay.text = "0";
+        scoreMultiplierDisplay.text = "x1";
     }
 }
